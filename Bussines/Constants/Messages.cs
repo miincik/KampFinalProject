430e83ecec0526e628712563b85e6548ee8f24f3
Mustafa incik <st4rs_33@hotmail.com>
2021-05-21 14:16:46 +0300

    Add project files.

---
+++ Bussines/Constants/Messages.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakım zamanı.";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten bir ürün mevcuttur";
+        public static string CategoryLimitExceded= "Kategori limiti aşıldı."  ;
+        public static string AuthorizationDenied = "Yetkiniz yok."  ;
+        public static string UserRegistered = "Kayıt bulunamadı.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Hatalı parola.";
+        public static string SuccessfulLogin = "Kayıt başarılı";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated  =  " Token oluşturuldu";
+    }
+}
---
+++ Bussines/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.CCS;
+using Business.Concrete;
+using Business.Abstract;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Http;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            //builder.RegisterType<HttpContextAccessor>().As<IHttpContextAccessor>();
+
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
